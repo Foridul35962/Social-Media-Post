36cec49510fd6a2de3ec33a0b2cddb5705fa264a
Foridul35962 <foridulislam35962@gmail.com>
2025-08-05 20:43:00 +0600

    update header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 const header = () => {
   return (
-    <div className="h-20 w-full bg-gray-800 px-3 sm:p-0">
+    <div className="h-16 w-full bg-gray-800 px-3 sm:p-0">
         <div className="container mx-auto w-full h-full flex items-center justify-between text-white">
             <div className="md:flex md:items-center md:gap-10 flex justify-between md:w-2/4">
                 <div className="text-3xl font-bold">Side Bar</div>
@@ -22,7 +22,7 @@ const header = () => {
                 </div>
             </div>
             <div className="flex gap-2">
-                <input type="text" className="border-2 border-white rounded-2xl"/>
+                <input type="text" className="border-2 border-white px-3 rounded-2xl"/>
                 <button className="border-2 p-1 rounded-xl cursor-pointer">Login</button>
                 <button className="bg-orange-600 p-1 rounded-xl cursor-pointer">Sign-up</button>
             </div>
