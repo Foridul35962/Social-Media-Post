00394a6034ea59fd42d63e65e97b2ea9d342aa35
Foridul35962 <foridulislam35962@gmail.com>
2025-08-07 07:46:14 +0600

    Side Bar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import Header from "./components/header"
+import SideBar from "./components/sideBar"
 
 const App = () => {
+    const [page, setPage]=useState("home");
   return (
     <div>
       <Header></Header>
+      <SideBar page={page} setPage={setPage}></SideBar>
     </div>
   )
 }
