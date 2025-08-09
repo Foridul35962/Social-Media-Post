2bd6655d70cf80a4b69840d153cdeb6f1d987e99
Foridul35962 <foridulislam35962@gmail.com>
2025-08-10 01:48:01 +0600

    change sidebar bg color using location

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 const sideBar = () => {
+  const location = useLocation();
+  const currentPath= location.pathname;
   return (
     <div className="h-svh bg-gray-800 py-6 px-3 text-center text-xl text-white">
       <div className="flex flex-col w-full gap-3">
-        <Link to="/" className={'bg-blue-600 w-full rounded-2xl p-1.5'}>
+        <Link to="/" className={`${currentPath==='/'? 'bg-blue-600': ""} w-full rounded-2xl p-1.5`}>
           Home
         </Link>
-        <Link to="/create-post" className={'bg-blue-600 w-full rounded-2xl py-1.5'}>
+        <Link to="/create-post" className={`${currentPath==='/create-post'? 'bg-blue-600': ""} w-full rounded-2xl py-1.5`}>
           Create Post
         </Link>
       </div>
